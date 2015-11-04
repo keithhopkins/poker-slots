78bc09f7436638b3f2722603fedae36dc582f7ba
topleft <pete.james.jeffryes@gmail.com>
2015-11-04 12:42:03 -0700

    deal and analyze hand tested

---
+++ client/test/mock-hands.js
@@ -0,0 +1,80 @@
+var junkHand = [
+    { code:'&#x1F0D8', weight: 8, name:'Eight of Clubs', suit:'Clubs'},
+    { code:'&#x1F0DB', weight: 11, name:'Jack of Clubs', suit:'Clubs'},
+    { code:'&#x1F0DA', weight: 10, name:'Ten of Clubs', suit:'Clubs'}, 
+    { code: '&#x1F0C1', weight: 1, name: "Ace of Diamonds", suit: 'Diamonds'},
+    { code: '&#x1F0C6', weight: 6, name: "Six of Diamonds", suit: 'Diamonds'}
+];
+
+var dupHand = [
+    { code:'&#x1F0D8', weight: 8, name:'Eight of Clubs', suit:'Clubs'},
+    { code:'&#x1F0D8', weight: 8, name:'Eight of Clubs', suit:'Clubs'},
+    { code:'&#x1F0DA', weight: 10, name:'Ten of Clubs', suit:'Clubs'}, 
+    { code: '&#x1F0C1', weight: 1, name: "Ace of Diamonds", suit: 'Diamonds'},
+    { code: '&#x1F0C6', weight: 6, name: "Six of Diamonds", suit: 'Diamonds'}
+];
+var pairHand = [
+    { code:'&#x1F0DE', weight: 13, name:'King of Clubs', suit:'Clubs'},
+    { code:'&#x1F0D8', weight: 8, name:'Eight of Clubs', suit:'Clubs'},
+    { code:'&#x1F0DA', weight: 10, name:'Ten of Clubs', suit:'Clubs'}, 
+    { code: '&#x1F0C1', weight: 1, name: "Ace of Diamonds", suit: 'Diamonds'},
+    { code:'&#x1F0D1', weight: 1, name:'Ace of Clubs', suit: 'Clubs'}
+    ];
+var twoPairHand = [
+    { code: '&#x1F0C8', weight: 8, name: "Eight of Diamonds", suit: 'Diamonds'},
+    { code:'&#x1F0D8', weight: 8, name:'Eight of Clubs', suit:'Clubs'},
+    { code:'&#x1F0DA', weight: 10, name:'Ten of Clubs', suit:'Clubs'}, 
+    { code: '&#x1F0C1', weight: 1, name: "Ace of Diamonds", suit: 'Diamonds'},
+    { code:'&#x1F0D1', weight: 1, name:'Ace of Clubs', suit: 'Clubs'}
+
+    ];
+var threeOfAKindHand = [    
+    { code:'&#x1F0DE', weight: 13, name:'King of Clubs', suit:'Clubs'},
+    { code:'&#x1F0DA', weight: 10, name:'Ten of Clubs', suit:'Clubs'}, 
+    { code: "&#x1F0B1", weight: 1, name: "Ace of Hearts", suit: "Hearts"},
+    { code: '&#x1F0C1', weight: 1, name: "Ace of Diamonds", suit: 'Diamonds'},
+    { code:'&#x1F0D1', weight: 1, name:'Ace of Clubs', suit: 'Clubs'}
+    ];
+var fourOfAKindHand = [
+    { code:'&#x1F0DE', weight: 13, name:'King of Clubs', suit:'Clubs'}, 
+    { code: "&#x1F0A1", weight: 1, name: "Ace of Spades", suit: "Spades"},
+    { code: "&#x1F0B1", weight: 1, name: "Ace of Hearts", suit: "Hearts"},
+    { code: '&#x1F0C1', weight: 1, name: "Ace of Diamonds", suit: 'Diamonds'},
+    { code:'&#x1F0D1', weight: 1, name:'Ace of Clubs', suit: 'Clubs'}
+    ];
+var straightHand = [
+    { code: "&#x1F0B1", weight: 1, name: "Ace of Hearts", suit: "Hearts"},
+    { code: "&#x1F0A2", weight: 2, name: "Two of Spades", suit: "Spades"},
+    { code: "&#x1F0A3", weight: 3, name: "Three of Spades", suit: "Spades"},
+    { code: "&#x1F0A4", weight: 4, name: "Four of Spades", suit: "Spades"},
+    { code: "&#x1F0A5", weight: 5, name: "Five of Spades", suit: "Spades"}
+    ];
+var fullHouseHand = [
+    { code: "&#x1F0B1", weight: 1, name: "Ace of Hearts", suit: "Hearts"},
+    { code: '&#x1F0C8', weight: 8, name: "Eight of Diamonds", suit: 'Diamonds'},
+    { code:'&#x1F0D8', weight: 8, name:'Eight of Clubs', suit:'Clubs'},
+    { code: '&#x1F0C1', weight: 1, name: "Ace of Diamonds", suit: 'Diamonds'},
+    { code:'&#x1F0D1', weight: 1, name:'Ace of Clubs', suit: 'Clubs'}
+
+    ];
+var flushHand = [
+    { code: "&#x1F0A1", weight: 1, name: "Ace of Spades", suit: "Spades"},
+    { code: "&#x1F0A2", weight: 2, name: "Two of Spades", suit: "Spades"},
+    { code: "&#x1F0A3", weight: 3, name: "Three of Spades", suit: "Spades"},
+    { code: "&#x1F0A4", weight: 4, name: "Four of Spades", suit: "Spades"},
+    { code: "&#x1F0A5", weight: 5, name: "Five of Spades", suit: "Spades"}
+    ];
+var straightFlushHand = [
+    { code: "&#x1F0A1", weight: 1, name: "Ace of Spades", suit: "Spades"},
+    { code: "&#x1F0A2", weight: 2, name: "Two of Spades", suit: "Spades"},
+    { code: "&#x1F0A3", weight: 3, name: "Three of Spades", suit: "Spades"},
+    { code: "&#x1F0A4", weight: 4, name: "Four of Spades", suit: "Spades"},
+    { code: "&#x1F0A5", weight: 5, name: "Five of Spades", suit: "Spades"}
+    ];
+var royalFlushHand = [
+    { code:'&#x1F0D1', weight: 1, name:'Ace of Clubs', suit:'Clubs'},
+    { code:'&#x1F0DA', weight: 10, name:'Ten of Clubs', suit:'Clubs'},
+    { code:'&#x1F0DB', weight: 11, name:'Jack of Clubs', suit:'Clubs'},
+    { code:'&#x1F0DD', weight: 12, name:'Queen of Clubs', suit:'Clubs'},
+    { code:'&#x1F0DE', weight: 13, name:'King of Clubs', suit:'Clubs'}
+];
